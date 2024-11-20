86d611a9821e52aed6f2d5926b44dc2777b227d9
davimdolabella <davimdolabella03@gmail.com>
2024-11-20 20:51:25 -0300

    print no django

--- firstContact/appTeste1/views.py
+++ firstContact/appTeste1/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def ver_produto(request):
-    return render(request, 'ver_produto.html')
+    return render(request, 'ver_produto.html',{'Nome':'Caio'})
