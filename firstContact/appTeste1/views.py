5520b0312a5c3bf8a417ca670207faae3f66d060
davimdolabella <davimdolabella03@gmail.com>
2024-11-21 20:48:48 -0300

    terminando first contact

--- firstContact/appTeste1/views.py
+++ firstContact/appTeste1/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Pessoa
 def ver_produto(request):
-    return render(request, 'ver_produto.html',{'Nome':'Caio'})
+    if request.method == "GET":
+        return render(request, 'ver_produto.html',{'Nome':'Caio'})
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+        
+        pessoa = Pessoa(nome=nome, idade=idade)
+        pessoa.save()
+        pessoas = Pessoa.objects.filter(idade=16)
+
+        return HttpResponse(pessoas)
+    
--- firstContact/django_teste/settings.py
+++ firstContact/django_teste/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'django_teste.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,6 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
