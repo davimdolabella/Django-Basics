aa7f67cf2aaed793edc49884a7880958a0012680
davimdolabella <davimdolabella03@gmail.com>
2024-12-07 10:30:30 -0300

    testes unitarios

--- cursodjango/recipes/tests.py
+++ cursodjango/recipes/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_recipes_home_is_correct(self):
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+    def test_recipes_category_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+    def test_recipes_detail_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
