a802c8f77f69712a89385ee443f559939320843b
davimdolabella <davimdolabella03@gmail.com>
2024-12-16 21:18:37 -0300

    mais testes e coverage

---
+++ cursodjango/recipes/tests/test_recipe_model_recipe.py
@@ -0,0 +1,53 @@
+from recipes.tests.test_recipe_base import RecipeTestBase, Recipe
+from django.core.exceptions import ValidationError
+from django.urls import reverse, resolve
+from recipes import views
+from parameterized import parameterized
+
+
+class RecipeModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    def make_recipe_no_default(self):
+            recipe = Recipe(
+                category=self.make_category(name='newCategory'),
+                author=self.make_author(username='newUser'),
+                title ='Recipe title',
+                description ='Recipe description',
+                slug ='recipe-slug',
+                preparation_time =10,
+                preparation_time_unit ='Minutos',
+                servings =5,
+                servings_unit ='Porções',
+                preparation_steps ='Preparation Steps',
+            )
+            recipe.full_clean()
+            recipe.save()
+            return recipe
+
+    @parameterized.expand([
+        ('title',65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit',65)
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+         setattr(self.recipe, field, 'a' * (max_length + 1) )
+         with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
+    
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_default()
+        self.assertFalse(recipe.preparation_steps_is_html)
+
+    def test_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_default()
+        self.assertFalse(recipe.is_published)
+
+    def test_recipe_string_representation(self):
+        self.recipe.title = 'Testing Representation'
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(str(self.recipe), 'Testing Representation')
--- cursodjango/recipes/tests/test_recipe_views.py
+++ cursodjango/recipes/tests/test_recipe_views.py
@@ -91,3 +91,12 @@ class RecipeViewsTest(RecipeTestBase):
         recipe= self.make_recipe(is_published=False)
         response = self.client.get(reverse('recipes:recipe', args=(recipe.id,)))
         self.assertEqual(response.status_code, 404)
+    
+    def test_recipr_search_uses_currect_view_function(self):
+        resolved = resolve(reverse('recipes:search'))
+        self.assertIs(resolved.func, views.search)
+    
+    def test_recipe_search_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:search'))
+        self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
